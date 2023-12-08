d82961493b27b1caa2a58b751e0bf7fb93ef955d
Assassin318554 <nahinsikdar123@gmail.com>
2023-12-09 01:06:15 +0600

    a

---
+++ Geek For Geeks/FirstNegativeIntInEveryWindowOfSizeK.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<long long> printFirstNegativeInteger(long long int A[], long long int N, long long int K){
+    int index = 0;
+    vector<long long> ans;
+    deque<long long> dq;
+
+    // Process first window of K size
+    for (int i = 0; i < K; i++){
+        if (A[i] < 0)
+            dq.push_back(i);
+    }
+
+    // store ans of first k size window
+    if (dq.size() > 0){
+        ans.push_back(A[dq.front()]);
+    }
+    else{
+        ans.push_back(0);
+    }
+
+    // now process for remaining windows
+    for (int i = K; i < N; i++){
+        // first pop out of window element
+        if (!dq.empty() && (i - dq.front()) >= K){
+            dq.pop_front();
+        }
+
+        // then push current element
+        if (A[i] < 0)
+            dq.push_back(i);
+
+        // put in ans
+        if (dq.size() > 0){
+            ans.push_back(A[dq.front()]);
+        }
+        else{
+            ans.push_back(0);
+        }
+    }
+    return ans;
+}
---
+++ Geek For Geeks/QueueReversal.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    queue<int> rev(queue<int> q){
+        stack<int> st;
+        while (!q.empty()){
+            st.push(q.front());
+            q.pop();
+        }
+        while (!st.empty()){
+            q.push(st.top());
+            st.pop();
+        }
+        return q;
+    }
+};
