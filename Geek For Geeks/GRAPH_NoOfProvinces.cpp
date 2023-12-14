b0cddf22094386b02d9a0adf5bc45e4b19f2faad
Assassin318554 <nahinsikdar123@gmail.com>
2023-12-14 08:17:32 +0600

    a

--- Geek For Geeks/GRAPH_NoOfProvinces.cpp
+++ Geek For Geeks/GRAPH_NoOfProvinces.cpp
@@ -1,44 +1,56 @@
-//{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
 
-
-// } Driver Code Ends
-//User function Template for C++
-
 class Solution {
   public:
-    int numProvinces(vector<vector<int>> adjList, int V) {
-        // code here
+    int NoOfProVinces = 0;
+    void DFS(unordered_map<int, vector<int>> &adjList, unordered_map<int, bool> &visited, int node){
+        visited[node] = true;
+        for(auto neighbour: adjList[node]){
+            if(!visited[neighbour]){
+                DFS(adjList, visited, neighbour);
+            }
+        }
+    }
+    int numProvinces(vector<vector<int>> adj, int V) {
+        unordered_map<int, vector<int>> adjList(V);
+        for(int i = 0; i < V; i++){
+            for(int j = 0; j < V; j++){
+                if(adj[i][j] == 1 && i != j){
+                    adjList[i].push_back(j);
+                    adjList[j].push_back(i);
+                }
+            } 
+        }
+        unordered_map<int, bool> visited;
+        for(int i = 0; i < V; i++){
+            if(!visited[i]){
+                NoOfProVinces++;
+                DFS(adjList, visited, i);
+            }
+        }
+        return NoOfProVinces;
     }
 };
 
-//{ Driver Code Starts.
 
 int main() {
     int t;
     cin >> t;
     while (t--) {
-        int V,x;
-        cin>>V;
-        
+        int V, x;
+        cin >> V;
         vector<vector<int>> adj;
-        
-        for(int i=0; i<V; i++)
-        {
+        for(int i = 0; i < V; i++){
             vector<int> temp;
-            for(int j=0; j<V; j++)
-            {
-                cin>>x;
+            for(int j = 0; j < V; j++){
+                cin >> x;
                 temp.push_back(x);
             }
             adj.push_back(temp);
         }
-        
-
         Solution ob;
         cout << ob.numProvinces(adj,V) << endl;
     }
     return 0;
 }
-// } Driver Code Ends
