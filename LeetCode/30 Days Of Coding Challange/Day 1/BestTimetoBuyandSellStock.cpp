e0b90f9792740c91d3bd406ed1c6c94daf0b3301
Assassin318554 <nahinsikdar123@gmail.com>
2024-09-13 15:50:27 +0600

    Day 1: Problem 5

--- LeetCode/30 Days Of Coding Challange/Day 1/BestTimetoBuyandSellStock.cpp
+++ LeetCode/30 Days Of Coding Challange/Day 1/BestTimetoBuyandSellStock.cpp
@@ -1,7 +1,29 @@
-/* 
-Problem Link: 
+/*
+Problem Link:
 https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/?utm_source=instabyte.io&utm_medium=referral&utm_campaign=interview-master-100
 */
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int maxProfit = 0;
+        int minPrice = INT_MAX;
+        int currProfit = 0;
+
+        for (auto currPrice : prices)
+        {
+            minPrice = min(minPrice, currPrice);
+
+            currProfit = (currPrice - minPrice);
+            if (currProfit > maxProfit)
+            {
+                maxProfit = currProfit;
+            }
+        }
+        return maxProfit;
+    }
+};
